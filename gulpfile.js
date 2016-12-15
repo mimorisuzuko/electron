947b393787603d59c5d92a25c60e30b7a16f0a0b
mimorisuzuko <hrky0206@gmail.com>
2016-12-15 16:29:45 +0900

    New

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,50 @@ const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const pug = require('gulp-pug');
+const path = require('path');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const watchify = require('watchify');
+const babel = require('gulp-babel');
+
+const src = 'src';
+const dst = 'dst/';
+const scssPath = path.join(src, '*.scss');
+const indexjsxPath = path.join(src, 'index.jsx');
+const jsxPath = path.join(src, '*.jsx');
+const pugPath = path.join(src, '*.pug');
 
 gulp.task('sass', () => {
-	gulp.src('src/*.scss')
+	gulp.src(scssPath)
 		.pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
 		.pipe(sass.sync())
-		.pipe(gulp.dest('./dst'));
+		.pipe(gulp.dest(dst));
+});
+
+gulp.task('jsx', () => {
+	process.env.NODE_ENV = 'production';
+	gulp.src(indexjsxPath)
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(babel({
+			presets: ['es2015', 'react']
+		}))
+		.on('error', (err) => console.log(`Error : ${err.message}`))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(dst));
 });
 
 gulp.task('pug', () => {
-	gulp.src('src/*.pug')
+	gulp.src(pugPath)
 		.pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
 		.pipe(pug({}))
-		.pipe(gulp.dest('./dst'));
+		.pipe(gulp.dest(dst));
 });
 
-gulp.task('watch', () => {
-	gulp.run('sass');
-	gulp.run('pug');
-	gulp.watch('src/*.scss', ['sass']);
-	gulp.watch('src/*.pug', ['pug']);
+gulp.task('watch', ['sass', 'jsx', 'pug'], () => {
+	gulp.watch(scssPath, ['sass']);
+	gulp.watch(jsxPath, ['jsx']);
+	gulp.watch(pugPath, ['pug']);
 });
 
-gulp.task('default', ['sass', 'pug']);
+gulp.task('default', ['sass', 'jsx', 'pug']);
