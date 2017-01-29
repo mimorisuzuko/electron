e88cbcfa45f8b2fe3474d3326b6c633858bfb7f7
mimorisuzuko <hrky0206@gmail.com>
2017-01-29 14:32:43 +0900

    Webpack

--- app/index.js
+++ app/index.js
@@ -1,24 +1,15 @@
-const electron = require('electron');
-const {BrowserWindow, app} = electron;
+const {app} = require('electron');
+const Single = require('./single-window');
+const single = new Single();
 
-let mainWindow = null;
-
-const createWindow = () => {
-	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600
-	});
-	mainWindow.loadURL(`file://${__dirname}/../dst/index.html`);
-	mainWindow.webContents.openDevTools();
-	mainWindow.on('closed', () => mainWindow = null);
-};
-
-app.on('ready', createWindow);
+app.on('ready', () => {
+	single.create();
+});
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') { app.quit(); }
 });
 
 app.on('activate', () => {
-	if (mainWindow === null) { createWindow(); }
+	if (!single.exist()) { single.create(); }
 });
