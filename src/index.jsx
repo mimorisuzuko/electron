8f6c94585a3d453ab37df692dc84566e25e0b8b5
mimorisuzuko <hrky0206@gmail.com>
2017-09-29 15:04:32 +0900

    Refactored

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import './index.scss';
 
 const $main = document.querySelector('main');
+const render = () => {
+	const { default: App } = require('./components/App');
 
-render(
-	<AppContainer>
-		<App />
-	</AppContainer>,
-	$main
-);
+	ReactDOM.render(
+		<AppContainer>
+			<App />
+		</AppContainer>,
+		$main
+	);
+};
 
+render();
 if (module.hot) {
-	module.hot.accept('./components/App', () => {
-		const { default: NextApp } = require('./components/App');
-
-		render(
-			<AppContainer>
-				<NextApp />
-			</AppContainer>,
-			$main
-		);
-	});
+	module.hot.accept(render);
 }
