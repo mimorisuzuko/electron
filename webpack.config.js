cb251cff39551a4f1de789d8256efec5d0c5898f
mimorisuzuko <hrky0206@gmail.com>
2019-02-27 18:10:23 +0900

    Use babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-const { DefinePlugin, HotModuleReplacementPlugin, LoaderOptionsPlugin } = require('webpack');
+const {
+	DefinePlugin,
+	HotModuleReplacementPlugin,
+	LoaderOptionsPlugin
+} = require('webpack');
 const libpath = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -6,8 +10,24 @@ module.exports = (env, { mode }) => {
 	const dst = 'app/dst';
 	const generateScopedName = '[name]__[local]_[hash:base64:5]';
 	const context = libpath.join(__dirname, 'src/');
-	const presets = ['react'];
+	const presets = ['@babel/react'];
 	const isProduction = mode === 'production';
+	const babelPlugins = [
+		['@babel/plugin-proposal-decorators', { legacy: true }],
+		['@babel/plugin-proposal-class-properties', { loose: false }],
+		[
+			'react-css-modules',
+			{
+				context,
+				generateScopedName,
+				filetypes: {
+					'.scss': {
+						syntax: 'postcss-scss'
+					}
+				}
+			}
+		]
+	];
 
 	const plugins = [
 		new CleanWebpackPlugin([dst], {
@@ -29,36 +49,38 @@ module.exports = (env, { mode }) => {
 	];
 
 	if (isProduction) {
-		presets.push(
-			[
-				'env', {
-					targets: {
-						chrome: 59
-					}
+		presets.push([
+			'@babel/env',
+			{
+				targets: {
+					chrome: 59
 				}
-			],
-			'stage-3'
+			}
+		]);
+		babelPlugins.push(
+			'@babel/plugin-syntax-dynamic-import',
+			'@babel/plugin-syntax-import-meta',
+			'@babel/plugin-proposal-json-strings'
 		);
 	} else {
+		babelPlugins.push('react-hot-loader/babel');
 		plugins.push(new HotModuleReplacementPlugin());
 	}
 
 	return {
 		context,
-		entry: isProduction ?
-			[
-				'babel-polyfill',
-				context
-			] : [
+		entry: isProduction
+			? ['@babel/polyfill', context]
+			: [
 				'webpack-dev-server/client?http://0.0.0.0:3000',
 				'webpack/hot/only-dev-server',
 				'react-hot-loader/patch',
 				context
-			],
+			  ],
 		output: {
 			path: libpath.join(__dirname, dst),
 			publicPath: 'http://localhost:3000/',
-			filename: 'index.js'
+			filename: '[name].js'
 		},
 		module: {
 			rules: [
@@ -69,30 +91,32 @@ module.exports = (env, { mode }) => {
 					options: {
 						babelrc: false,
 						presets,
-						plugins: [
-							'transform-decorators-legacy',
-							['react-css-modules',
-								{
-									context,
-									generateScopedName,
-									filetypes: {
-										'.scss': {
-											syntax: 'postcss-scss'
-										}
-									}
-								}
-							]
-						]
+						plugins: babelPlugins
 					}
 				},
 				{
 					test: /\.scss$/,
 					use: [
-						'style-loader',
-						`css-loader?importLoader=1&modules&localIdentName=${generateScopedName}`,
-						'postcss-loader',
-						'sass-loader'
+						{
+							loader: 'style-loader'
+						},
+						{
+							loader: 'css-loader',
+							options: {
+								importLoaders: 1,
+								modules: true,
+								localIdentName: generateScopedName
+							}
+						},
+						{
+							loader: 'postcss-loader'
+						},
+						{ loader: 'sass-loader' }
 					]
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
 				}
 			]
 		},
@@ -100,16 +124,17 @@ module.exports = (env, { mode }) => {
 			extensions: ['.js', '.jsx']
 		},
 		plugins,
-		node: {
-			__filename: false,
-			__dirname: false
-		},
-		target: 'electron-renderer',
 		devServer: {
 			hot: true,
 			port: 3000,
 			host: '0.0.0.0',
 			contentBase: libpath.join(__dirname, dst)
+		},
+		optimization: {
+			splitChunks: {
+				name: 'vendor',
+				chunks: 'initial'
+			}
 		}
 	};
 };
